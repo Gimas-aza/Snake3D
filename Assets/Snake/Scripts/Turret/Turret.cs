b71b09a9d0f4c06c5660831cf81f33818808f91a
GIMAS <gimas0micro@outlook.com>
2023-08-03 18:17:29 +0300

    Import model and Setting

--- Assets/Snake/Scripts/Turret/Turret.cs
+++ Assets/Snake/Scripts/Turret/Turret.cs
@@ -66,7 +66,7 @@ public class Turret : MonoBehaviour
         {
             if (enemy == null)
             {
-                _enemies.Remove(enemy);
+                // _enemies.Remove(enemy);
                 continue;
             }
 
